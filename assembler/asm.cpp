e5fe95f1094ed787e163a9a913db1b42dbc67285
Eliyah Hawila <eliyahhawila@gmail.com>
2023-07-19 16:34:39 -0400

    c-instruction maps added

--- assembler/asm.cpp
+++ assembler/asm.cpp
@@ -15,10 +15,16 @@ map<string, int> predefined = {{"R0", 0}, {"R1", 1}, {"R2", 2}, {"R3", 3}, {"R4"
 map<string,int>::iterator it = predefined.begin();
 
 
-map<string, string> comp1 = {};
-map<string, string> comp2 = {};
-map<string, string> dest = {};
-map<string, string> jump = {};
+map<string, string> comp0 = {{"0", "101010"}, {"1", "111111"}, {"-1", "111010"}, {"D", "001100"}, {"A", "110000"}, {"!D", "001101"}, {"!A", "110001"},
+                             {"-D", "001111"}, {"-A", "110011"}, {"D+1", "011111"}, {"A+1", "110111"}, {"D-1", "001110"}, {"A-1", "110010"}, {"D+A", "000010"},
+                             {"D-A", "010011"}, {"A-D", "000111"}, {"D&A", "000000"}, {"D|A", "010101"}};
+
+map<string, string> comp1 = {{"M", "110000"}, {"!M", "110001"}, {"-M", "110011"}, {"M+1", "110111"}, {"M-1", "110010"}, {"D+M", "000010"}, {"D-M", "010011"}, 
+                             {"M-D", "000111"}, {"D&M", "000000"}, {"D|M", "010101"}};
+
+map<string, string> dest = {{"M", "001"}, {"D", "010"}, {"DM", "011"}, {"A", "100"}, {"AM", "101"}, {"AD", "110"}, {"ADM", "111"}};
+
+map<string, string> jump = {{"JGT", "001"}, {"JEQ", "010"}, {"JGE", "011"}, {"JLT", "100"}, {"JNE", "101"}, {"JLE", "110"}, {"JMP", "111"}};
 
 string decTo15BitBinary(int n) {
 	string address;
