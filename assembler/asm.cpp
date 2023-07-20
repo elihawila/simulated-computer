b8ad30f9bdd4776056da8c654ce051c7e08e5356
Eliyah Hawila <eliyahhawila@gmail.com>
2023-07-19 20:01:38 -0400

    c-instruction handler added, ready for testing functions

--- assembler/asm.cpp
+++ assembler/asm.cpp
@@ -8,24 +8,48 @@
 #include <boost/lexical_cast.hpp>
 using namespace std;
 
+//predefined symbols table and its iterator
 map<string, int> predefined = {{"R0", 0}, {"R1", 1}, {"R2", 2}, {"R3", 3}, {"R4", 4}, {"R5", 5}, {"R6", 6}, {"R7", 7}, {"R8", 8}, {"R9", 9},
                                {"R10", 10}, {"R11", 11}, {"R12", 12}, {"R13", 13}, {"R14", 14}, {"R15", 15}, {"SCREEN", 16384}, {"KBD", 24576},
                                {"SP", 0}, {"LCL", 1}, {"ARG", 2}, {"THIS", 3}, {"THAT", 4}};
-
 map<string,int>::iterator it = predefined.begin();
 
 
+
+
+
+//compute table for a = 0 and its iterator
 map<string, string> comp0 = {{"0", "101010"}, {"1", "111111"}, {"-1", "111010"}, {"D", "001100"}, {"A", "110000"}, {"!D", "001101"}, {"!A", "110001"},
                              {"-D", "001111"}, {"-A", "110011"}, {"D+1", "011111"}, {"A+1", "110111"}, {"D-1", "001110"}, {"A-1", "110010"}, {"D+A", "000010"},
                              {"D-A", "010011"}, {"A-D", "000111"}, {"D&A", "000000"}, {"D|A", "010101"}};
+map<string,string>::iterator comp0_it = comp0.begin();
 
+
+
+
+//compute table for a = 1 and its iterator
 map<string, string> comp1 = {{"M", "110000"}, {"!M", "110001"}, {"-M", "110011"}, {"M+1", "110111"}, {"M-1", "110010"}, {"D+M", "000010"}, {"D-M", "010011"}, 
                              {"M-D", "000111"}, {"D&M", "000000"}, {"D|M", "010101"}};
+map<string,string>::iterator comp1_it = comp1.begin();
 
+
+
+
+//destination table and its iterator
 map<string, string> dest = {{"M", "001"}, {"D", "010"}, {"DM", "011"}, {"A", "100"}, {"AM", "101"}, {"AD", "110"}, {"ADM", "111"}};
+map<string,string>::iterator dest_it = dest.begin();
 
+
+
+
+//jump table and its iterator
 map<string, string> jump = {{"JGT", "001"}, {"JEQ", "010"}, {"JGE", "011"}, {"JLT", "100"}, {"JNE", "101"}, {"JLE", "110"}, {"JMP", "111"}};
 
+map<string,string>::iterator jump_it = jump.begin();
+
+
+
+//decimal to binary converter
 string decTo15BitBinary(int n) {
 	string address;
 	// Size of an integer is assumed to be 15 bits
@@ -41,6 +65,7 @@ string decTo15BitBinary(int n) {
 	return address;
 }
 
+//in-line white space cleaner and comment remover
 string cleaner(string line) {
 	//erasing all whitespace in the line
 	line.erase(remove_if(line.begin(), line.end(), ::isspace), line.end());
@@ -56,6 +81,7 @@ string cleaner(string line) {
 	return line;
 }
 
+//empty line remover
 vector<string> parser(string filename) {
 	vector<string> grabber;
 	string line;
@@ -68,14 +94,14 @@ vector<string> parser(string filename) {
 		if (!clean_line.empty()) {
 			//appending lines to grabber for element by element processing
 			grabber.push_back(clean_line);
-            cout << clean_line << endl;
+            //cout << clean_line << endl;
         }
     }
 	ss.close();
 	return grabber;
 }
 
-
+//first pass to process labels
 string firstPass(string line, int index) {
 	if (line.at(0) == 40) {
 		line.erase(line.begin());
@@ -86,6 +112,7 @@ string firstPass(string line, int index) {
 }
 
 
+//second pass to process variables
 string secondPass(string line, int index) {
 	string symbol;
 	int data_memory = 16;
@@ -93,7 +120,7 @@ string secondPass(string line, int index) {
 		for (int i = 1; i < line.length(); i++) {
 			symbol.push_back(line.at(i));
 		}
-		if (predefined.find(symbol) != predefined.end()) {
+		if (!(predefined.find(symbol) != predefined.end())) {
 			predefined.insert( it, pair<string, int>(symbol, data_memory));
 			data_memory++;
 		}
@@ -101,6 +128,8 @@ string secondPass(string line, int index) {
 	return symbol;
 }
 
+
+//third pass to change the results of the affected line in the first and second pass into binary
 string thirdPass(string line, int index) {
 	string address;
 	string address_str;
@@ -127,21 +156,81 @@ string thirdPass(string line, int index) {
 	return line;
 }
 
+
+//c-instruction handler from instruction to binary
 string cInstruction(string line) {
 	string init = "111";
 	string a;
 	string c;
 	string d;
 	string j;
+	char j_char = 59;
+	char e_char = 61;
+	auto jmp = find(line.begin(), line.end(), j_char);
+	auto equal = find(line.begin(), line.end(), e_char);
+	if(!isdigit(line.at(0)) && line.at(0) != 64 && line.at(0) != 40) {
+		if (!(jmp != line.end())) {
+			j = "000";
+			for (int id = 0; id < (equal - line.begin()); id++) {
+				d.push_back(line.at(id));
+			}
+			if (dest.find(d) != dest.end()) {
+				d = dest[d];
+			}
+			for (int  ic = (equal - line.begin()) + 1; ic < line.length(); ic++) {
+				c.push_back(line.at(ic));
+			}
+			if (comp0.find(c) != comp0.end()) {
+				c = comp0[c];
+				a = "0";
+			}
+			else if (comp1.find(c) != comp1.end()) {
+				c = comp1[c];
+				a = "1";
+			}
+		}
+		else {
+			if (!(equal != line.end())) {
+				d = "000";
+				for (int ic = 0; ic < (jmp - line.begin()); ic++) {
+					c.push_back(line.at(ic));
+				}
+				for (int ij = (jmp - line.begin()) + 1; ij < line.length(); ij++) {
+					j.push_back(line.at(ij));
+				}
+				j = jump[j];
+				if (comp0.find(c) != comp0.end()) {
+					c = comp0[c];
+					a = "0";
+				}
+				else if (comp1.find(c) != comp1.end()) {
+					c = comp1[c];
+					a = "1";
+				}
+			}
+		}
+	}
+	line = init + a + c + d + j;
 	return line;
 }
 
+
+//main function packager
 int main() {
 	cout << endl << "file to assemble >> ";
 	string filename;
 	cin >> filename;
-	//vector<string> grabber;
-	//grabber = parser(filename);
-
+	string line;
+	vector<string> grabber;
+	grabber = parser(filename);
+	for (int i = 0; i < grabber.size(); i++) {
+		//line = grabber[i];
+		//line = firstPass(line, i);
+		//line = secondPass(line, i);
+		//line = thirdPass(line, i);
+		//line = cInstruction(line);
+		grabber[i] = line;
+		cout << grabber[i] << endl;
+	}
 	return 0;
 }
